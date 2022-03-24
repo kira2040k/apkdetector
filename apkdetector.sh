031f43e88b2a6e0ed4ad0985a9637b91f46a0739
kira2040k <46848151+kira2040k@users.noreply.github.com>
2022-03-24 11:44:18 +0300

    Update apkdetector.sh

--- apkdetector.sh
+++ apkdetector.sh
@@ -2,13 +2,13 @@ JAVA=$(strings $1 | grep "META-INF/kotlin-stdlib")
 if [  "$JAVA" ]
 then
     echo "JAVA"
-    echo "for decompile https://github.com/skylot/jadx"
+    
 fi
 REACT=$(strings $1 | grep "reactnative") 
 if [  "$REACT" ]
 then
     echo "REACT native javascript"    
-    echo "for decompile https://www.npmjs.com/package/react-native-decompiler" 
+    
 fi
 flutter=$(strings $1 | grep "libflutter.so") 
 if [  "$flutter" ]
@@ -20,5 +20,5 @@ xamarin=$(strings $1 | grep "libxamarin-app")
 if [  "$xamarin" ]
 then
     echo "xamarin C#"     
-    echo "for decompile https://github.com/dnSpy/dnSpy"
+   
 fi
